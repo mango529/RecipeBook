7dddd2d2e586f1cf095a428a52e1e753350b13ff
winter28 <leegoeun1218@naver.com>
2020-12-09 00:29:20 +0900

    timer 종료, 취소

--- app/src/main/java/ddwu/mobile/finalproject/ma01_20180988/FragmentTimer.java
+++ app/src/main/java/ddwu/mobile/finalproject/ma01_20180988/FragmentTimer.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import java.text.ParseException;
@@ -21,9 +22,10 @@ import java.util.TimerTask;
 
 public class FragmentTimer extends Fragment {
     NumberPicker npHour, npMinute, npSecond;
-    Button btnTimerStart;
+    Button btnTimerStart,  btnTimerStop, btnTimerCancel, btnTimerContinue;
     TextView tvTimer;
     CountDownTimer countDownTimer;
+    ConstraintLayout clTimerSet, clTimerStart;
 
     int hour, minute, second;
 
@@ -34,7 +36,12 @@ public class FragmentTimer extends Fragment {
         npMinute = view.findViewById(R.id.npMinute);
         npSecond = view.findViewById(R.id.npSecond);
         btnTimerStart = view.findViewById(R.id.btnTimerStart);
+        btnTimerStop = view.findViewById(R.id.btnTimerStop);
+        btnTimerCancel = view.findViewById(R.id.btnTimerCancel);
+        btnTimerContinue = view.findViewById(R.id.btnTimerContinue);
         tvTimer = view.findViewById(R.id.tvTimer);
+        clTimerSet = view.findViewById(R.id.clTimerSet);
+        clTimerStart = view.findViewById(R.id.clTimerStart);
 
         npHour.setMaxValue(12);
         npHour.setMinValue(0);
@@ -47,29 +54,34 @@ public class FragmentTimer extends Fragment {
         btnTimerStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 피커 숨기고 tvTimer, 정지, 취소, 재시작 버튼 띄우기
+                clTimerSet.setVisibility(View.INVISIBLE);
+                clTimerStart.setVisibility(View.VISIBLE);
                 hour = npHour.getValue();
                 minute = npMinute.getValue();
                 second = npSecond.getValue();
-                long millis = dateToMill(String.format("%2d:%2d:%2d", hour, minute, second));
+                long millis = dateToMill(String.format("%02d : %02d : %02d", hour, minute, second));
 
                 countDownTimer = new CountDownTimer(millis,1000) {
 
                     @Override
                     public void onTick(long millisUntilFinished) {
-                        tvTimer.setText(String.format("%2d:%2d:%2d", hour, minute, second));
+                        tvTimer.setText(String.format("%02d : %02d : %02d", hour, minute, second));
                         if (second == 0 ) {
                             minute--;
+                            second = 59;
                         }
                         if (second == 0 && minute == 0) {
                             hour--;
+                            second = 59;
                         }
                         second--;
                     }
 
                     @Override
                     public void onFinish() {
-                        // 피커 띄우고 알림 보내기
+                        // 알림 보내기
+                        clTimerSet.setVisibility(View.VISIBLE);
+                        clTimerStart.setVisibility(View.INVISIBLE);
                         Toast.makeText(getContext(), "타이머 종료", Toast.LENGTH_SHORT).show();
                     }
                 };
@@ -77,26 +89,20 @@ public class FragmentTimer extends Fragment {
             }
         });
 
-
-
-
-//        TimerTask timerTask = new TimerTask() {
-//            int hour = npHour.getValue();
-//            int minute = npMinute.getValue();
-//            int second = npSecond.getValue();
-//
-//            @Override
-//            public void run() {
-//                tvTimer.setText(String.format("%2d : %2d : %2d", hour, minute, second));
-//
-//            }
-//        };
+        btnTimerCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countDownTimer.cancel();
+                clTimerSet.setVisibility(View.VISIBLE);
+                clTimerStart.setVisibility(View.INVISIBLE);
+            }
+        });
 
         return view;
     }
 
     public long dateToMill(String date) {
-        String pattern = "HH:mm:ss";
+        String pattern = "HH : mm : ss";
         SimpleDateFormat formatter = new SimpleDateFormat(pattern);
         Date trans_date = null;
         try {
