a05353e0eabbaff37d66977c784cd6faf4241eff
winter28 <leegoeun1218@naver.com>
2020-12-23 00:54:32 +0900

    내 레시피 보기

---
+++ app/src/main/java/ddwu/mobile/finalproject/ma01_20180988/MyRecipeDetailActivity.java
@@ -0,0 +1,103 @@
+package ddwu.mobile.finalproject.ma01_20180988;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class MyRecipeDetailActivity extends AppCompatActivity {
+    private static final String TAG = "MyRecipeDetailActivity";
+
+    ImageView ivDetailMyRcpImg;
+    TextView tvDetailMyRcpName, tvMyRcpDate, tvMyRcpMemo, tvMyRcpHashtag;
+    ListView lvDetailMyIngre;
+    ViewPager vpMyManual;
+    RatingBar rbRating;
+
+    ArrayAdapter ingredientAdapter;
+    ManualAdapter manualAdapter;
+    ImageFileManager imageFileManager;
+    NetworkManager networkManager;
+    Recipe recipe;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_recipe_detail);
+
+        recipe = (Recipe) getIntent().getSerializableExtra("recipe");
+
+        ivDetailMyRcpImg = findViewById(R.id.ivDetailMyRcpImg);
+        tvDetailMyRcpName = findViewById(R.id.tvDetailMyRcpName);
+        tvMyRcpDate = findViewById(R.id.tvMyRcpDate);
+        tvMyRcpMemo = findViewById(R.id.tvMyRcpMemo);
+        tvMyRcpHashtag = findViewById(R.id.tvMyRcpHashtag);
+        lvDetailMyIngre = findViewById(R.id.lvDetailMyIngre);
+        vpMyManual = findViewById(R.id.vpMyManual);
+        rbRating = findViewById(R.id.rbRating);
+
+        ingredientAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, recipe.getIngredients());
+        manualAdapter = new ManualAdapter(this, recipe.getManuals());
+        imageFileManager = new ImageFileManager(this);
+        networkManager = new NetworkManager(this);
+
+        lvDetailMyIngre.setAdapter(ingredientAdapter);
+        vpMyManual.setAdapter(manualAdapter);
+
+        tvDetailMyRcpName.setText(recipe.getName());
+        Bitmap savedBitmap = imageFileManager.getBitmapFromTemporary(recipe.getImageLink());
+        if (savedBitmap != null) {
+            ivDetailMyRcpImg.setImageBitmap(savedBitmap);
+            Log.d(TAG, "Image loading from file");
+        } else {
+            new GetImageAsyncTask().execute(recipe.getImageLink());
+            Log.d(TAG, "Image loading from network");
+        }
+        rbRating.setRating(recipe.getRating());
+        if (recipe.getDate() != null) {
+            tvMyRcpDate.setText(recipe.getDate());
+        }
+        if (recipe.getMemo() != null) {
+            tvMyRcpMemo.setText(recipe.getMemo());
+        }
+        if (recipe.getHashtag() != null) {
+            tvMyRcpHashtag.setVisibility(View.VISIBLE);
+            tvMyRcpHashtag.setText("#" + recipe.getHashtag());
+        }
+    }
+
+    class GetImageAsyncTask extends AsyncTask<String, Void, Bitmap> {
+        String imageAddress;
+
+        public GetImageAsyncTask() {
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            imageAddress = params[0];
+            Bitmap result = null;
+            result = networkManager.downloadImage(imageAddress);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (bitmap != null) {
+                ivDetailMyRcpImg.setImageBitmap(bitmap);
+                imageFileManager.saveBitmapToTemporary(bitmap, imageAddress);
+            }
+        }
+    }
+}
